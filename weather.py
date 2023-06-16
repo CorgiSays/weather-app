aa717d8ab2ea5b4f36050ac33b55353aea142aef
CorgiSays <132166223+CorgiSays@users.noreply.github.com>
2023-06-16 10:00:10 -0700

    Update weather.py
    asdf

--- weather.py
+++ weather.py
@@ -1,9 +1,13 @@
 import requests
 import os
 from gtts import gTTS
+from time import sleep
 
-dataarcadi = requests.get("https://api.weatherapi.com/v1/current.json?q=V5R&key=dc065743d1ee4ff1b37161302231606&lang=%2Fapplication.json").json()
-text = "The temperature right now is " + str(dataarcadi.get("current").get("temp_c"))
-
-gTTS(text=text,lang="en",slow=False).save("temperature.mp3")
-os.system("start temperature.mp3")
+def playTemp():
+    dataa = requests.get("https://api.weatherapi.com/v1/current.json?q=V5R&key=dc065743d1ee4ff1b37161302231606&lang=%2Fapplication.json").json()
+    text = "The temperature right now is " + str(dataa.get("current").get("temp_c")) + ", it is currently " + str(dataa.get("current").get("condition").get("text")) + " The wind is currently " + str(dataa.get("wind_kph")) + " kilometers per hour and it is travelling at" + str(dataa.get("wind_degree"))
+    gTTS(text=text,lang="en",slow=False).save("temperature.mp3")
+    os.system("start temperature.mp3")
+    sleep(15)
+    playTemp()
+playTemp()
